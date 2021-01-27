5f9d280941961d08b190afb1770871791187c5e4
Natarajan Vijayaraman <nvijayaraman@mac-mini.lan>
2021-01-26 22:44:13 -0800

    reducing load to 2M records-fix

--- webapp.py
+++ webapp.py
@@ -16,7 +16,7 @@ def load_data():
     #accidents_df_3 = pd.read_csv('./US_Accidents_CleanedUp_Jan26-3.csv.gz')
     #accidents_df_4 = pd.read_csv('./US_Accidents_CleanedUp_Jan26-4.csv.gz')
     #accidents_df_5 = pd.read_csv('./US_Accidents_CleanedUp_Jan26-5.csv.gz')
-    return pd.concat([accidents_df_1, accidents_df_2, accidents_df_3, accidents_df_4, accidents_df_5], axis=0)
+    return pd.concat([accidents_df_1, accidents_df_2], axis=0)
 
 accidents_df = load_data()
 state_count_df = pd.DataFrame(accidents_df['state'].value_counts().head(15))
